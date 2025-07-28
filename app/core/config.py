74bf1b0d0849a2a4955b12412d07d09d11ba8c19
Ivan Saydyashev <isarus2007@mail.ru>
2025-07-28 20:05:59 +0300

    feature: Redis database infrastructure

--- app/core/config.py
+++ app/core/config.py
@@ -8,13 +8,20 @@ class Settings(BaseSettings):
     PAYMENT_PROVIDER_TOKEN: str = Field()
     WEBHOOK_SECRET: str = Field()
     PAYLOAD_SECRET: str = Field()
+
     POSTGRES_USER: str = Field()
     POSTGRES_DB: str = Field()
     POSTGRES_PASSWORD: str = Field()
     POSTGRES_HOST: str = Field()
     POSTGRES_PORT: str = Field()
 
+    REDIS_DB: int = Field()
+    REDIS_PASSWORD: str = Field()
+    REDIS_HOST: str = Field()
+    REDIS_PORT: str = Field()
+
     DATABASE_URL: str = Field(default="")
+    REDIS_URL: str = Field(default="")
 
     class Config:
         env_file = str(Path("__file__").cwd() / ".env")
@@ -22,10 +29,14 @@ class Settings(BaseSettings):
     def __init__(self, **data):
         super().__init__(**data)
         # Генерируем DATABASE_URL после загрузки остальных полей
-        self.DATABASE_URL = self._generate_db_url()
+        self.DATABASE_URL = self._generate_postgres_db_url()
+        self.REDIS_URL = self._generate_redis_db_url()
     
-    def _generate_db_url(self) -> str:
+    def _generate_postgres_db_url(self) -> str:
         return (
             f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
             f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
         )
+
+    def _generate_redis_db_url(self) -> str:
+        return f"redis://:{self.REDIS_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
